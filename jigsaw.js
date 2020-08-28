ddfe44576e335c9fde68c64649be5f885c594569
Greg Harley <greg@harleyhood.com>
2020-08-28 09:35:22 -0600

    refactor

--- jigsaw.js
+++ jigsaw.js
@@ -1,10 +1,11 @@
 function save(filename, data) {
-    var blob = new Blob([data], { type: "text/csv" });
+    const blob = new Blob([data], { type: "text/csv" });
+
     if (window.navigator.msSaveOrOpenBlob) {
         window.navigator.msSaveBlob(blob, filename);
     }
     else {
-        var elem = window.document.createElement('a');
+        let elem = window.document.createElement('a');
         elem.href = window.URL.createObjectURL(blob);
         elem.download = filename;
         document.body.appendChild(elem);
@@ -13,39 +14,41 @@ function save(filename, data) {
     }
 }
 
-var seed = 1;
+let seed = 1;
 const MAX_JITTER = 0.13;
-const random = () => { var x = Math.sin(seed) * 10000; seed += 1; return x - Math.floor(x); }
+const random = () => { let x = Math.sin(seed) * 10000; seed += 1; return x - Math.floor(x); }
 const rbool = () => { return random() > 0.5; }
 
 const $ = (id) => { return document.getElementById(id); }
 const resetValue = (id) => {
     $(id).value = $(id).dataset["default"];
-    update_range(id);
+    updateRange(id);
 }
-const update_range = (id) => {
+const updateRange = (id) => {
     $("_" + id).value = $(id).value + (id === "seed" ? "" : "%");
     if (id === "taboffset") $("randomoffset").checked = false;
     if (id === "jitter") $("randomjitter").checked = false;
     update();
 }
-const update_text = (id) => { let val = parseFloat($("_" + id).value); if (!isNaN(val)) { $(id).value = val; } update_range(id); }
+const updateText = (id) => { let val = parseFloat($("_" + id).value); if (!isNaN(val)) { $(id).value = val; } updateRange(id); }
 
-var tabSize, taboffset, randomOffset = false, jitter, randomJitter = false, flip, xn, yn, vertical, offset, width, height, radius;
+function genTab(x, y, props, isVertical = false) {
+    const {width, height, offset, randomJitter, randomOffset, taboffset, tabSize, xn, yn} = props;
+    let {jitter} = props;
 
-function gen_tab(x, y, isVertical = false) {
     let xSize = width / xn;
     let ySize = height / yn;
     let xOffset = offset + xSize * x;
     let yOffset = offset + ySize * y;
     let result = "";
-    let b, c, d, e;
+    let b, c, d, e, flip;
 
     const uniform = () => {
         if (randomJitter) {
-            jitter = MAX_JITTER * random();
+            jitter = MAX_JITTER * random() * (rbool() ? 1.0 : -1.0);
         }
-        var r = random(); return -jitter + r * jitter * 2;
+
+        return -jitter + random() * jitter * 2;
     }
     const next = () => { flip = rbool(); b = uniform(); c = uniform(); d = uniform(); e = uniform(); }
     const lValue = (value) => {
@@ -67,7 +70,7 @@ function gen_tab(x, y, isVertical = false) {
 
     // There are 3 curves to a tab, each curve is defined by 3 points but we only need to provide 2 because the first point is the current position
     let points = [];
-    var xDisplace = randomOffset ? 0.5 + ((tabSize - MAX_JITTER * 2) * random() * (rbool() ? 1.0 : -1.0)) : taboffset;
+    let xDisplace = randomOffset ? 0.5 + ((tabSize - MAX_JITTER * 2) * random() * (rbool() ? 1.0 : -1.0)) : taboffset;
 
     // First curve
     points.push({ x: lValue(xDisplace + b + d), y: wValue(-tabSize + c) });
@@ -79,7 +82,7 @@ function gen_tab(x, y, isVertical = false) {
     points.push({ x: lValue(0.8), y: wValue(e) });
     points.push({ x: lValue(1.0), y: wValue(0.0) });
 
-    for (var idx = 0; idx < points.length; ++idx) {
+    for (let idx = 0; idx < points.length; ++idx) {
         if (idx % 2 === 0) {
             result += "S ";
         }
@@ -89,28 +92,19 @@ function gen_tab(x, y, isVertical = false) {
 
         result += xVal + " " + yVal + " ";
     }
-    next();
+
     return result;
 }
 
-function gen_d() {
-    seed = parseInt($("seed").value);
-    randomJitter = $("randomjitter").checked;
-    randomOffset = $("randomoffset").checked;
-    jitter = parseFloat($("jitter").value) / 100.0;
-    xn = parseInt($("xn").value);
-    yn = parseInt($("yn").value);
-    tabSize = parseFloat($("tabsize").value) / 200.0;
-    taboffset = parseFloat($("taboffset").value) / 100.0;
-
-    let xi, yi, str = "";
+function genD(props) {
+    const {xn, yn, offset, radius, width, height} = props;
+    let xi, yi, str = "", vertical = 0;
 
     // Draw horizontal lines
-    vertical = 0;
     for (yi = 1; yi < yn; ++yi) {
         xi = 0;
         for (; xi < xn; ++xi) {
-            str += gen_tab(xi, yi);
+            str += genTab(xi, yi, props);
         }
     }
 
@@ -119,7 +113,7 @@ function gen_d() {
     for (xi = 1; xi < xn; ++xi) {
         yi = 0;
         for (; yi < yn; ++yi) {
-            str += gen_tab(xi, yi, true);
+            str += genTab(xi, yi, props, true);
         }
     }
 
@@ -133,50 +127,64 @@ function gen_d() {
     str += "A " + (radius) + " " + (radius) + " 0 0 1 " + (offset) + " " + (offset + height - radius) + " ";
     str += "L " + (offset) + " " + (offset + radius) + " ";
     str += "A " + (radius) + " " + (radius) + " 0 0 1 " + (offset + radius) + " " + (offset) + " ";
+
     return str;
 }
 
-const getRadius = () => {
-    width = parseInt($("width").value);
-    height = parseInt($("height").value);
-    radius = parseFloat($("radius").value);
+const getProps = () => {
+    const props = {};
 
-    const minSize = Math.min(width, height) / 2.0;
+    props.width = parseInt($("width").value);
+    props.height = parseInt($("height").value);
+    props.radius = parseFloat($("radius").value);
+    props.jitter = parseFloat($("jitter").value) / 100.0;
+    props.randomJitter = $("randomjitter").checked;
+    props.randomOffset = $("randomoffset").checked;
+    props.tabSize = parseFloat($("tabsize").value) / 200.0;
+    props.taboffset = parseFloat($("taboffset").value) / 100.0;
+    props.xn = parseInt($("xn").value);
+    props.yn = parseInt($("yn").value);
 
-    if (radius > minSize) {
-        radius = minSize;
-        $("radius").value = radius;
+    const minSize = Math.min(props.width, props.height) / 2.0;
+
+    if (props.radius > minSize) {
+        props.radius = minSize;
+        $("radius").value = props.radius;
     }
+
+    seed = parseInt($("seed").value);
+
+    return props;
 }
 
 function update() {
-    getRadius();
-    offset = 5.5;
+    const props = getProps();
+    props.offset = 5.5;
 
-    var ratio = 1.0 * width / height;
+    let ratio = 1.0 * props.width / props.height;
     if (ratio > 1.5) {
-        radius = radius * 900 / width;
-        width = 900;
-        height = width / ratio;
+        props.radius = props.radius * 900 / props.width;
+        props.width = 900;
+        props.height = props.width / ratio;
     }
     else {
-        radius = radius * 600 / height;
-        height = 600;
-        width = height * ratio;
+        props.radius = props.radius * 600 / props.height;
+        props.height = 600;
+        props.width = props.height * ratio;
     }
-    $("puzzlecontainer").setAttribute("width", width + 11);
-    $("puzzlecontainer").setAttribute("height", height + 11);
-    $("puzzlepath").setAttribute("d", gen_d());
+    $("puzzlecontainer").setAttribute("width", props.width + 11);
+    $("puzzlecontainer").setAttribute("height", props.height + 11);
+    $("puzzlepath").setAttribute("d", genD(props));
 }
 
 function generate() {
-    getRadius();
-    offset = 0.0;
+    const props = getProps();
+    props.offset = 0.0;
 
-    var data = "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.0\" ";
-    data += "width=\"" + width + "mm\" height=\"" + height + "mm\" viewBox=\"0 0 " + width + " " + height + "\">";
+    let data = "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.0\" ";
+    data += "width=\"" + props.width + "mm\" height=\"" + props.height + "mm\" viewBox=\"0 0 " + props.width + " " + props.height + "\">";
     data += "<path fill=\"none\" stroke=\"black\" stroke-width=\"0.1\" d=\"";
-    data += gen_d();
+    data += genD(props);
     data += "\"></path></svg>";
 
     save("jigsaw.svg", data);
